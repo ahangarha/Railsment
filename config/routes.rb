32246fae55b24245373767afc4fbd898ed79b297
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-27 16:12:32 +0300

    add basic UI and splash screen for guest users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :groups, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "groups#index"
 end
