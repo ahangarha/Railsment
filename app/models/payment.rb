f1409c5fa19ebee45c112fca62d039ba7e410524
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-27 13:53:04 +0300

    add model validation for payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,7 @@ class Payment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :group_payments
   has_many :groups, through: :group_payments
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
