9d56374586bc9fedaf4dcae5ef51ff57ef50e465
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-27 21:23:10 +0300

    add link to create new category

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups, only: [:index, :show]
+  resources :groups, only: [:index, :show, :new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
