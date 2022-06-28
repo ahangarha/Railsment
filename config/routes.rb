9086cde31bc812f2fad001af031f74beb7fb57ee
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-28 16:22:03 +0300

    create payment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups, only: [:index, :show, :new, :create]
-  resources :payments, only: [:new] 
+  resources :payments, only: [:new, :create] 
 
   # Defines the root path route ("/")
   root "groups#index"
